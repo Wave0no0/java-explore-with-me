666afc65615ead89bf265c7308151af8bd31d4dd
wave <vlad3056968@yandex.ru>
2025-07-17 22:16:32 +0300

    Health check added

--- main-service/src/test/java/ru/practicum/ewm/stats/StatsClientWireMockTest.java
+++ main-service/src/test/java/ru/practicum/ewm/stats/StatsClientWireMockTest.java
@@ -3,6 +3,7 @@ package ru.practicum.ewm.stats;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
